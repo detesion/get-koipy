7d18c67767f3ab8d5f720d26232d4d0a013ed914
mayone <95861898+detesion@users.noreply.github.com>
2024-10-02 01:16:16 +0800

    Update koipy-docker.sh

--- koipy-docker.sh
+++ koipy-docker.sh
@@ -93,11 +93,11 @@ configure_bot () {
         printf "请输入 Slave Comment(后端展示名):"
         read -r slave_comment <&1
         printf "是否启用 Sub-Store(默认不启用false):"
-        read -r sub_store_enable <&1
-        sub_store_enable=${sub_store_enable:-"false"}
+        read -r substore_enable <&1
+        substore_enable=${substore_enable:-"false"}
         printf "是否自动部署 Sub-Store(默认不启用false):"
-        read -r sub_store_autoDeploy <&1
-        sub_store_autoDeploy=${sub_store_autoDeploy:-"false"}
+        read -r substore_autoDeploy <&1
+        substore_autoDeploy=${substore_autoDeploy:-"false"}
 
 
         # 更新 config.yaml 文件
@@ -122,8 +122,8 @@ configure_bot () {
             sed -i.bak "s|^\(      path: \).*|\1$slave_path|" ./koipy/config.yaml
             sed -i.bak "s|^\(      comment: \).*|\1\"$slave_comment\"|" ./koipy/config.yaml
             sed -i.bak "s|^\(  substore:\)|\1|" ./koipy/config.yaml
-            sed -i.bak "s|^\(    enable: \).*|\1\"$sub_store_enable\"|" ./koipy/config.yaml
-            sed -i.bak "s|^\(    autoDeploy: \).*|\1\"$sub_store_autoDeploy\"|" ./koipy/config.yaml
+            sed -i.bak "s|^\(  enable: \).*|\1$substore_enable|" ./koipy/config.yaml
+            sed -i.bak "s|^\(  autoDeploy: \).*|\1$substore_autoDeploy|" ./koipy/config.yaml
             echo "config.yaml 已更新。"
         else
             echo "缺少必要的配置文件，退出。"
